d5e0a7d67d94b4af8171c1074919acdd6d30a4f5
7oDA22 <34072752+7oDA22@users.noreply.github.com>
2018-10-23 09:33:51 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -392,7 +392,7 @@ if (message.content.startsWith(prefix + "uptime")) {
 });
 
 client.on('ready', () => {
-  client.user.setGame(` 2!help :gem: `,'https://www.twitch.tv/v5bz');
+  client.user.setGame(` 2!help ♥ `,'https://www.twitch.tv/v5bz');
   console.log('---------------');
   console.log('Desert Bot Is Online')
   console.log('---------------')
